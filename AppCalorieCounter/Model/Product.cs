55b6c0a4da85d290d6b1c3c91cb342ad6c80141d
Vyacheslav <slava-morozov-1999@mail.ru>
2025-11-28 22:32:05 +0300

    Добавлен UI для вкладки "Расчет калорий в продукте"
    UI готов для вкладки "Добавление в мои продукты"
    Начал работу над UI вкладки "Обзор Мои продукты"

---
+++ AppCalorieCounter/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCalorieCounter.Model
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Product_quantity { get; set; }
+        public int Caloric_in_100_units_of_mass { get; set; } //Калорийность продукта на 100 едениц массыы.Еденицы массы либо Мл либо грамм
+    }
+}
